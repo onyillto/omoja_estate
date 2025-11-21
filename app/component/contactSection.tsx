5f967660b4609d880262f333a9099301a71a3d77
onyillto <56700691+onyillto@users.noreply.github.com>
2025-11-21 11:47:23 +0100

    Update contactSection.tsx

--- app/component/contactSection.tsx
+++ app/component/contactSection.tsx
@@ -62,7 +62,7 @@ const ContactSection = () => {
         </svg>
       ),
       title: "Email",
-      info: "info@mojarealestatelimited.com",
+      info: "mojaealestate1@gmail.com",
     },
     {
       icon: (
@@ -81,7 +81,26 @@ const ContactSection = () => {
         </svg>
       ),
       title: "Phone",
-      info: "+234 XXX XXX XXXX",
+      info: "+2349168366266",
+    },
+    {
+      icon: (
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+          />
+        </svg>
+      ),
+      title: "Whatsapp Number",
+      info: "+2349168366266",
     },
   ];
 
